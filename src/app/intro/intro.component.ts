a23727e1151579ff352e6715873ce982ee72ca49
Mohammad Ahmed <ansarimohammadahmed72@gmail.com>
2020-05-16 13:39:26 +0530

    [Animation On Scroll]

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,36 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, useAnimation} from '@angular/animations';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { trigger, transition, useAnimation, state, style, animate} from '@angular/animations';
 import { bounce, pulse, bounceIn, flipInX } from 'ng-animate';
+import { from } from 'rxjs';
+
+const style1 = style({
+  opacity: 1,
+  transform: "translateX(0)"
+})
+
+const style2 = style({
+  opacity: 0,
+  transform: "translateX(-100%)"
+})
+const style3 = style({
+  opacity: 0,
+  transition: "translateX(50%)"
+})
+const style4 = style({
+  opacity:1,
+  transition: "translateX(100%)"
+})
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.css'],
   animations: [
-    trigger('bounce', [transition('* => *', useAnimation(bounce, {
-      // Set the duration to 1seconds and delay to 1seconds
-      params: { timing: 1, delay: 1 }
-    }))])
+    trigger('foobar', [
+      state('show', style1),
+      state('hide', style2),
+      transition('show => hide', animate('5s ease-out')),
+      transition('hide => show', animate('1s ease-in'))
+    ])
   ],
-})
+  })
 export class IntroComponent implements OnInit {
-  bounce = true;
-  constructor() { }
+  state = 'hide';
+  
+
+  constructor(public el: ElementRef) { }
 
   ngOnInit(): void {
   }
-  onScrollEvent(){
-    console.log('test');
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+    if (scrollPosition >= componentPosition) { //110
+      this.state = 'show';
+      
+    } else {
+      console.log('I am in else')
+      this.state = 'hide';
+      
+    }
+
   }
-
-
-
-
-
-
-
-
-
-
-
+   
 }
